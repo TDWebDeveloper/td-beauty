fae203a96f25a341b972d955d6d9ecca7df7c1c1
TDWebDeveloper <146351858+TDWebDeveloper@users.noreply.github.com>
2023-11-03 15:34:40 -0500

    Adicion del CSS enlazado por SASS e inclusión de su enlace por PHP

--- blocksy-child/functions.php
+++ blocksy-child/functions.php
@@ -5,3 +5,11 @@ if (! defined('WP_DEBUG')) {
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 });
+
+/*	Add New Stylesheet	*/        
+add_action( 'wp_enqueue_scripts', 'childtheme_sass_styles');
+function childtheme_sass_styles() {
+ // enqueue style
+ wp_register_style('style-sass', get_template_directory_uri().'-child/css/style.css');
+ wp_enqueue_style('style-sass');
+}
