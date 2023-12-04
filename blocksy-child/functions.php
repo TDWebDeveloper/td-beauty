4eb5dedc73b90ea62f9740c1e3ae99a53a85e3c6
TDWebDeveloper <146351858+TDWebDeveloper@users.noreply.github.com>
2023-12-04 12:02:56 -0500

    Actualizar functions.php 04/12/23

--- blocksy-child/functions.php
+++ blocksy-child/functions.php
@@ -13,3 +13,14 @@ function childtheme_sass_styles() {
  wp_register_style('style-sass', get_template_directory_uri().'-child/css/style.css');
  wp_enqueue_style('style-sass');
 }
+/*	Add New JavaScript	*/
+function enqueue_scripts_child_theme(){
+   wp_enqueue_script( 'text', get_stylesheet_directory_uri() . '/js/text.js', array('jquery'), '1.0.0', true );
+}
+add_action('wp_enqueue_scripts', 'enqueue_scripts_child_theme');
+/*	LatePoint	*/
+function latepoint_text_validation(){
+	echo '<div class="os-col-12"><h3 class="latepoint-desc-title titulo-cancellation">Cancellation Policy</h3>
+	<p class="desc-cancellation">If you need to cancel or reschedule your appointment please let us know 24 hours prior otherwise the full treatment cost will be charged to the card on file.</p></div>';
+}
+add_action('latepoint_booking_steps_contact_after','latepoint_text_validation', 5);
